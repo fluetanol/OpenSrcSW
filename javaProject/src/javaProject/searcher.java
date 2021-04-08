89607136ac8890469b7b7715ab4386bf219d634f
fluetanol <mike415415@naver.com>
2021-04-08 18:04:40 +0900

    commit the searcher.java

---
+++ javaProject/src/javaProject/searcher.java
@@ -0,0 +1,17 @@
+package javaProject;
+
+public class searcher {
+	
+	searcher(){};
+	
+	void queryreading(String args1,String args2,int num)
+	{
+		
+	}
+	
+	private float CalcSim()
+	{
+		return 0;
+	}
+	
+}
