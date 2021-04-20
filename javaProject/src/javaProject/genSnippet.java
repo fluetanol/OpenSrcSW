e3f1b1d66e49bace6739c25939f528a84522c285
fluetanol <mike415415@naver.com>
2021-04-20 18:57:00 +0900

    genSnimppet

---
+++ javaProject/src/javaProject/genSnippet.java
@@ -0,0 +1,48 @@
+package javaProject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class genSnippet {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		if(args[0].equals("-f"))
+		{
+			File test = new File("C:\\Users\\mike4\\OneDrive\\Desktop\\코딩\\SimpleIR\\javaProject\\src\\input.txt");
+			FileReader read = new FileReader(test);
+			
+			String [] keyword = args[3].split(" ");
+			int count []  = new int[keyword.length];
+
+			
+			char buf[] = new char[80];
+			read.read(buf);
+			String[] bufstr = buf.toString().split(" ");
+			
+			for(int i=0; i<keyword.length; i++)
+			if(bufstr[i].equals(keyword[i]))
+				count[i]++;
+			
+			int max;
+			int maxindex;
+			
+			for(int i=0; i<keyword.length; i++)
+				for(int j=0; j<keyword.length; j++)
+				{
+					
+					
+				}
+		}
+		
+			
+		
+	
+	
+			
+		}
+	}
+
